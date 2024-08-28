7fd9a7ae9b67fbdb030cef89f98e99975bdf08cf
gastonfuentes <gastonnicolasfuentes@gmail.com>
2024-08-28 20:32:10 -0300

    se agrego conexion con supabase y se muestran datos de db

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,20 @@
 import { titleFont } from "@/config/fonts";
+import { createClientServer } from "@/utils/supabase/server";
 
-export default function Home() {
+export default async function Home() {
+
+  const supabase = createClientServer()
+
+  const { data: generos } = await supabase.from('generos').select('id, gender')
+  const { data: categorias } = await supabase.from('categoriasTalampaya').select()
+  const { data: parejas } = await supabase.from('parejas').select()
+
+
   return (
     <div className="">
       <h1>hola mundo</h1>
       <h1 className={`${titleFont.className} font-bold`}>hola mundo</h1>
+      <pre> {JSON.stringify(parejas, null, 2)}</pre>
     </div>
   );
 }
